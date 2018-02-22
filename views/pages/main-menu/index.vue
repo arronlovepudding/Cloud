93b1a128deb9927a99ed7f162c83b610e0cf7c84
llb421270473 <421270473@qq.com>
2018-02-23 00:40:13 +0800

    add user manage

--- views/pages/main-menu/index.vue
+++ views/pages/main-menu/index.vue
@@ -2,6 +2,10 @@
   <el-menu :default-active="activeIndex" class="main-menu" :style="componentsStyle" mode="horizontal"
            @select="handleSelect">
     <span class="logo-area">Cloud</span>
+    <el-submenu index="2">
+      <template slot="title">用户管理</template>
+      <el-menu-item index="2-1">退出</el-menu-item>
+    </el-submenu>
     <el-submenu index="1">
       <template slot="title">项目管理</template>
       <el-menu-item index="1-1">项目列表</el-menu-item>
@@ -29,8 +33,12 @@
     beforeDestroy () {
     },
     methods: {
-      handleSelect () {
-        this.$router.push('/')
+      handleSelect (key, keyPath) {
+        let path = {
+          '1-1': '/',
+          '2-1': '/login/out'
+        }[key]
+        this.$router.push(path)
       }
     },
     computed: {},
