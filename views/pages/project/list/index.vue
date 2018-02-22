252e3da774d8155b97c0a760397e54d6362939ff
llb421270473 <421270473@qq.com>
2018-02-22 09:44:01 +0800

    update views project list

--- views/pages/project/list/index.vue
+++ views/pages/project/list/index.vue
@@ -1,23 +1,42 @@
 <template>
-
+  <el-row>
+    <el-col :span="4">
+      <div class="project" @click="showDetail(item.id)" v-for="item in list">
+        {{item.name}}
+      </div>
+    </el-col>
+  </el-row>
 </template>
 <style lang="scss" scoped>
   @import './index.scss';
 </style>
 <script>
+  import Api from '../../../utils/api-tools'
+
   export default {
     data () {
       return {
+        list: []
       }
     },
     components: {},
     created () {
     },
     mounted () {
+      this.init()
     },
     beforeDestroy () {
     },
-    methods: {},
+    methods: {
+      init () {
+        Api.get('/api/project/list').then(res => {
+          this.list = res.data || []
+        })
+      },
+      showDetail (id) {
+        this.$router.push(`/project/detail/${id}`)
+      }
+    },
     computed: {},
     watch: {}
   }
