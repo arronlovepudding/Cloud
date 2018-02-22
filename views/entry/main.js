22677bf5384dde3eb42dd017d8ce65c18f1450fe
llb421270473 <421270473@qq.com>
2018-02-23 00:18:39 +0800

    update init Api

--- views/entry/main.js
+++ views/entry/main.js
@@ -3,11 +3,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from '../app.vue'
 import {createRouter} from '../router'
+import ApiTools from '../utils/api-tools'
 
 Vue.use(ElementUI)
 
 export function createApp () {
   const router = createRouter()
+  ApiTools.createAPI(router)
   const app = new Vue({
     router,
     render: h => h(App)
