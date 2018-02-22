970ae2c494d6a53e9b2d1034b151b0ea1b09b273
llb421270473 <421270473@qq.com>
2018-02-22 14:25:40 +0800

    remove some item

--- views/pages/main-menu/index.vue
+++ views/pages/main-menu/index.vue
@@ -5,7 +5,6 @@
     <el-submenu index="1">
       <template slot="title">项目管理</template>
       <el-menu-item index="1-1">项目列表</el-menu-item>
-      <el-menu-item index="1-2">新增项目</el-menu-item>
     </el-submenu>
   </el-menu>
 </template>
