7bcbcab04a38846377b4fc8d4185821978936323
llb421270473 <421270473@qq.com>
2018-02-23 00:22:29 +0800

    add middlewares and JWT check

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
-
+const config = require('config')
 const middleware = require('./middlewares')
 const routers = require('./routers')
+
+const koaJWT = require('koa-jwt')
+const jwtSecret = config.get('jwt.secret')
 
 // error handler
 onerror(app)
@@ -19,6 +22,7 @@ app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/dist'))
 app.use(require('koa-static')(__dirname + '/public'))
+app.use(middleware.util)
 
 // logger
 app.use(async (ctx, next) => {
@@ -28,6 +32,15 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+// JWT check
+app.use(koaJWT({secret: jwtSecret}).unless((ctx) => {
+  if (/^\/api/.test(ctx.path)) {
+    return ['/api/login'].includes(ctx.path)
+  }
+  return true
+}))
+
+// routers
 app.use(routers.api.routes(), routers.api.allowedMethods())
 app.use(routers.cloud.routes(), routers.cloud.allowedMethods())
 app.use(middleware.view(app))
