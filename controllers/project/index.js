822334acd6236f99e46e4877186af593691b7979
llb421270473 <421270473@qq.com>
2018-02-22 14:27:08 +0800

    add show and update method

--- controllers/project/index.js
+++ controllers/project/index.js
@@ -3,7 +3,6 @@ const ProjectDetail = require('../../models').ProjectDetail
 
 exports.list = async function (ctx, next) {
   let result = await Project.find({})
-  console.log(result)
   ctx.body = result.map(item => {
     return {
       id: item._id,
@@ -13,15 +12,16 @@ exports.list = async function (ctx, next) {
 }
 
 exports.detail = async function (ctx, next) {
-  let projectId = ctx.query.projectId
-  let result = await ProjectDetail.find({projectId})
-  console.log(result)
+  let projectId = ctx.params.projectId
+  let result = await ProjectDetail.find({projectId}).sort({create_at: -1})
   ctx.body = result.map(item => {
     return {
       id: item._id,
       name: item.name,
       path: item.path,
-      address: `/cloud/${item.projectId}/${item.path}`
+      address: `/cloud/${item.projectId}/${item.path}`,
+      pvCount: item.pvCount,
+      create_at: item.create_at_date
     }
   })
 }
@@ -29,7 +29,7 @@ exports.detail = async function (ctx, next) {
 exports.create = async function (ctx, next) {
   let params = ctx.request.body || {}
   let instance = new ProjectDetail()
-  instance.projectId = params.projectId
+  instance.projectId = ctx.params.projectId
   instance.name = params.name
   instance.path = params.path.replace(/^\//, '')
   instance.content = params.content
@@ -37,10 +37,36 @@ exports.create = async function (ctx, next) {
   ctx.status = 204
 }
 
+exports.show = async function (ctx, next) {
+  let _id = ctx.params.itemId
+  let result = await ProjectDetail.findOne({_id})
+  ctx.body = result
+}
+
+exports.update = async function (ctx, next) {
+  let _id = ctx.params.itemId
+  let params = ctx.request.body || {}
+  await ProjectDetail.update({
+    _id
+  }, {
+    $set: {
+      name: params.name,
+      content: params.content
+    }
+  })
+  ctx.status = 204
+}
+
 exports.content = async function (ctx, next) {
   let projectId = ctx.params.projectId
   let path = ctx.params.path
   let result = await ProjectDetail.findOne({projectId, path}) || {}
-  console.log(result)
+  await ProjectDetail.update({
+    _id: result._id
+  }, {
+    $inc: {
+      pvCount: 1
+    }
+  })
   ctx.body = result.content
 }
