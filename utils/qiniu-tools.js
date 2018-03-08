13d36d72aecc5ab3cd7cc59514e283d5d7d43805
llb421270473 <421270473@qq.com>
2018-03-08 16:05:16 +0800

    add config qiniu

--- utils/qiniu-tools.js
+++ utils/qiniu-tools.js
@@ -3,7 +3,7 @@ const qiniu = require('qiniu')
 qiniu.conf.ACCESS_KEY = config.qiniu.ak
 qiniu.conf.SECRET_KEY = config.qiniu.sk
 
-const bucketName = 'i200-static'
+const bucketName = config.qiniu.bucket
 
 exports.uptoken = function (keyPath) {
   var putPolicy = new qiniu.rs.PutPolicy({scope: bucketName, saveKey: `${keyPath}$(etag)$(ext)`})
