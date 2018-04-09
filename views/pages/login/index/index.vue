0bd90fec1849b7bfd49e65d89cb0f676a1d1d160
llb421270473 <421270473@qq.com>
2018-04-09 14:32:46 +0800

    update login view

--- views/pages/login/index/index.vue
+++ views/pages/login/index/index.vue
@@ -1,19 +1,47 @@
 <template>
-  <el-row>
-    <el-col :span="8" :offset="8">
-      <el-form ref="form" label-width="80px">
-        <el-form-item label="用户名">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-          <el-input type="password" v-model="form.password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="login">登录</el-button>
-        </el-form-item>
-      </el-form>
-    </el-col>
-  </el-row>
+  <div class="login-page">
+    <div class="login-container">
+      <div class="login-panel">
+        <div class="login-brand">Cloud</div>
+        <div class="login-content">
+          <div class="account-input">
+            <input ref="formName" type="email" class="login-input" value="" name="account" placeholder="用户名"
+                   v-model="form.name" @focus="closeFormValidate('name')">
+            <div class="input-errorMask" v-show="formValidate.name"
+                 @click="closeFormValidate('name','formName')">请输入手机号或邮箱
+            </div>
+          </div>
+          <div class="password-input">
+            <input ref="formPass" type="password" class="login-input" value="" name="password" placeholder="密码"
+                   v-model="form.password" @focus="closeFormValidate('password')"
+                   @keyup.enter="login">
+            <div class="input-errorMask" v-show="formValidate.password"
+                 @click="closeFormValidate('password','formPass')">请输入密码
+            </div>
+          </div>
+          <button class="login-btn s-btn--orange" @click="login">登录</button>
+          <!--
+          <div class="cooperation">
+            <div class="title-cooperation">
+              <span class="tit">使用第三方账号登录</span>
+            </div>
+            <div class="cooperation-content">
+              <a href="">
+                <i class="icon-weixin"></i>
+              </a>
+              <a href="">
+                <i class="icon-weibo"></i>
+              </a>
+              <a href="">
+                <i class="icon-qq"></i>
+              </a>
+            </div>
+          </div>
+          -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
   @import './index.scss';
@@ -29,6 +57,10 @@
         form: {
           name: '',
           password: ''
+        },
+        formValidate: {
+          name: false,
+          password: false
         }
       }
     },
@@ -40,7 +72,24 @@
     beforeDestroy () {
     },
     methods: {
+      beforeLogin () {
+        if (this.form.name === '') {
+          this.formValidate.name = true
+          return true
+        }
+        if (this.form.password === '') {
+          this.formValidate.password = true
+          return true
+        }
+        return false
+      },
+      closeFormValidate (validateName, formName = null) {
+        this.formValidate[validateName] = false
+        if (formName === null) return
+        this.$refs[formName].focus()
+      },
       login () {
+        if (this.beforeLogin()) return
         Api.post(`/api/login`, this.form).then(res => {
           let data = res.data || {}
           let token = data.token
